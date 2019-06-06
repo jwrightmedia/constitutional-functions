f61ecfde7ab7c0d73efde710db62cb8524617f00
Josh Wright <josh-wright@Joshs-MacBook-Pro.local>
2019-06-06 15:38:35 -0500

    Add wp_head clean up

--- sample.php
+++ sample.php
@@ -150,4 +150,88 @@ add_filter( 'wp_check_filetype_and_ext', function($filetype_ext_data, $file, $fi
 // Register Custom Navigation Walker - https://github.com/wp-bootstrap/wp-bootstrap-navwalker
 require_once('wp_bootstrap_navwalker.php');
 
+//Clean up wp_head
+
+//Remove JQuery migrate
+
+function remove_jquery_migrate( $scripts ) {
+	if ( ! is_admin() && isset( $scripts->registered['jquery'] ) ) {
+	$script = $scripts->registered['jquery'];
+		if ( $script->deps ) { // Check whether the script has any dependencies
+		     $script->deps = array_diff( $script->deps, array( 'jquery-migrate' ) );
+		}
+	}
+}
+add_action( 'wp_default_scripts', 'remove_jquery_migrate' );
+
+function disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );        
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );          
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+	add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
+}
+add_action( 'init', 'disable_emojis' );
+
+/**
+* Filter function used to remove the tinymce emoji plugin.
+*
+ * @param    array  $plugins 
+ * @return   array  Difference betwen the two arrays
+*/
+
+function disable_emojis_tinymce( $plugins ) {
+	if ( is_array( $plugins ) ) {
+	    return array_diff( $plugins, array( 'wpemoji' ) );
+	}
+	return array();
+}
+
+/**
+* Remove emoji CDN hostname from DNS prefetching hints.
+*
+* @param  array  $urls          URLs to print for resource hints.
+* @param  string $relation_type The relation type the URLs are printed for.
+* @return array                 Difference betwen the two arrays.
+*/
+
+function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+	if ( 'dns-prefetch' == $relation_type ) {
+	    // Strip out any URLs referencing the WordPress.org emoji location
+	    $emoji_svg_url_bit = 'https://s.w.org/images/core/emoji/';
+	    foreach ( $urls as $key => $url ) {
+	        if ( strpos( $url, $emoji_svg_url_bit ) !== false ) {
+	            unset( $urls[$key] );
+	        }
+	    }
+	}
+	return $urls;
+}
+
+remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
+remove_action( 'wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed
+remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
+remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
+remove_action( 'wp_head', 'index_rel_link' ); // index link
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // start link
+remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
+remove_action( 'wp_head', 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
+remove_action( 'wp_head', 'rest_output_link_wp_head');
+remove_action( 'wp_head', 'wp_oembed_add_discovery_links');
+remove_action( 'template_redirect', 'rest_output_link_header', 11 );
+
+function my_deregister_scripts(){
+	wp_deregister_script( 'wp-embed' );
+}
+add_action( 'wp_footer', 'my_deregister_scripts' );
+
+function remove_block_css(){
+	wp_dequeue_style( 'wp-block-library' );
+}
+add_action( 'wp_enqueue_scripts', 'remove_block_css', 100 );
 ?>
